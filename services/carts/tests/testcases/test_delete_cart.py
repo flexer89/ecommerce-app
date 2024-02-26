7438a7643cd197ba215edb920bbce0378a105137
flexer89 <olsza.op@gmail.com>
2024-02-26 23:26:02 +0100

    WIP unittests in products service

---
+++ services/carts/tests/testcases/test_delete_cart.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from src.app import app
+
+client = TestClient(app)
+
+
+@patch("src.routes.redis_client")
+def test_delete_cart(mock_redis_client):
+    mock_redis_client.delete.return_value = 1
+
+    response = client.delete("/delete", params={"email": "test@example.com"})
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+    mock_redis_client.delete.assert_called_once_with("email:test@example.com")
---
+++ services/carts/tests/testcases/test_remove_from_cart.py
@@ -0,0 +1,40 @@
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from src.app import app
+
+client = TestClient(app)
+
+
+@patch("src.routes.redis_client")
+def test_remove_and_delete_from_cart(mock_redis_client):
+    mock_redis_client.hgetall.return_value = {"product1": "1", "product2": "1"}
+
+    response = client.post("/remove", params={"email": "test@example.com", "product_id": "product1"})
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+    mock_redis_client.hgetall.assert_called_once_with("email:test@example.com")
+    mock_redis_client.hdel.assert_called_once_with("email:test@example.com", "product1")
+    
+@patch("src.routes.redis_client")
+def test_remove_from_cart(mock_redis_client):
+    mock_redis_client.hgetall.return_value = {"product1": "2", "product2": "1"}
+
+    response = client.post("/remove", params={"email": "test@example.com", "product_id": "product1"})
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+    mock_redis_client.hgetall.assert_called_once_with("email:test@example.com")
+    mock_redis_client.hdel.assert_not_called()
+    
+@patch("src.routes.redis_client")
+def test_remove_nonexistent_cart(mock_redis_client):
+    mock_redis_client.hgetall.return_value = {}
+    
+    response = client.post("/remove", params={"email": "test@example.com", "product_id": "product1"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Product not found in cart"}
+    mock_redis_client.hgetall.assert_called_once_with("email:test@example.com")
+    mock_redis_client.hdel.assert_not_called()
+    
+                                            
