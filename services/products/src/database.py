7011bdd79efa8c7430fe80a68a875e3bef04b727
Jakub Olszak <olsza.op@gmail.com>
2024-03-10 11:17:26 +0100

    fix mypy for products service

--- services/products/src/database.py
+++ services/products/src/database.py
@@ -3,9 +3,9 @@ from mongomock import MongoClient as MockMongoClient
 import os
 
 if os.getenv("ENV") == "test":
-    mongo_client = MockMongoClient()
+    mongo_client = MockMongoClient() # type: ignore
 else:
-    mongo_client = MongoClient("mongodb://products-db-service:27017/")
+    mongo_client = MongoClient("mongodb://products-db-service:27017/") # type: ignore
     
 db = mongo_client["products_db"]
 collection = db["products"]
